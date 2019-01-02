1d3d15a4a53e3ed54a20265824eda61c705f58eb
Sergio Rodriguez Guasch <sergi9rr9r@gmail.com>
2019-01-02 11:31:30 +0100

    Update gitignore, add master.bib

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # LaTeX dependencies
-sudo apt-get install texlive-latex-base
+sudo apt-get install texlive-latex-base -y
 
-sudo apt-get install texlive-fonts-recommended
-sudo apt-get install texlive-fonts-extra
-
-sudo apt-get install texlive-latex-extra
+sudo apt-get install texlive-fonts-recommended -y
+sudo apt-get install texlive-fonts-extra -y
+# Extra LaTeX packages
+sudo apt-get install texlive-latex-extra -y
+# LaTeX "make" utility
+sudo apt-get install latexmk -y
