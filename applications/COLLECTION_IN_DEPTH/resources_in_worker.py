ff511d6aea481de36ff56f1187a6872e25e8f447
Sergio Rodriguez Guasch <sergi9rr9r@gmail.com>
2019-03-19 09:49:31 +0100

    Fixed depth example

--- applications/COLLECTION_IN_DEPTH/resources_in_worker.py
+++ applications/COLLECTION_IN_DEPTH/resources_in_worker.py
@@ -21,17 +21,18 @@ def generate_object(seed):
   np.random.seed(seed)
   return np.random.rand(5)
 
-@task(c = {type: COLLECTION_IN, depth: 4}, returns = 1)
+@task(c = {Type: COLLECTION_IN, Depth: 4}, returns = 1)
 def select_element(c, i, j, k, l):
   """Given a collection and a set of indices, return the element
   of this collection which corresponds to these indices
   """
+  print("Matrix content is %s" % str(c))
   return c[i][j][k][l]
 
 def main():
   from pycompss.api.api import compss_wait_on
   # Generate a 2x2x2x2 random matrix with pre-determined seed
-  two_two_two_two_matrix = 
+  two_two_two_two_matrix = \
     [
       [
         [
@@ -43,7 +44,7 @@ def main():
     ]
   # Pick the fifth vector from a COLLECTION_IN parameter
   zero_one_zero_one = compss_wait_on(select_element(two_two_two_two_matrix, 0, 1, 0, 1))
-  print("My chosen vector is \t %s" % str( fifth_vector ))
+  print("My chosen vector is \t %s" % str( zero_one_zero_one ))
 
   # Recreate this vector locally 
   import numpy as np
@@ -53,7 +54,10 @@ def main():
   print("The correct vector is \t %s" % str( master_vector ))
 
   # Check that they match
-  assert np.allclose(fifth_vector, master_vector), "Vectors do not match"
+  assert np.allclose(zero_one_zero_one, master_vector), "Vectors do not match"
+
+  import time
+  time.sleep(1238472487238)
 
   # They did match, we can now end the program
   print("Congratulations! Both vectors match!")
